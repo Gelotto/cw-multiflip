3ad318f13ad81d0dec83784b3f1a27adbe1137c5
Daniel Gabriele <dg0offset@gmail.com>
2023-04-26 20:05:21 -0400

    clean up

--- src/execute/mod.rs
+++ src/execute/mod.rs
@@ -1,9 +1,9 @@
+mod configure_nois;
 mod flip_coins;
 mod receive_randomness;
 mod set_owner;
-mod use_nois;
 
+pub use configure_nois::configure_nois;
 pub use flip_coins::flip_coins;
 pub use receive_randomness::receive_randomness;
 pub use set_owner::set_owner;
-pub use use_nois::configure_nois;
